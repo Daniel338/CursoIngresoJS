8e88f9d526e8abc036f7dd3c5f47656cab1857e3
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 05:50:10 -0300

    E/S Ejerc 06

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+  let num1 = document.getElementById('txtIdNumeroUno').value;
+  let num2 = document.getElementById('txtIdNumeroDos').value;
+
+  // Conversion
+  num1 = parseInt(num1,10);
+  num2 = parseInt(num2,10);
+  
+  alert(`La suma es ${num1 + num2}`);
 }
 
