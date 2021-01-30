3d0e8c2ba9e4497af6ee67f6c60bf7224f499da9
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 15:01:52 -0300

    Instr IF 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+    let random = Math.random() * 10;
+    random = Math.floor(random + 1);
+    console.log(random);
+    
+    if (random == 9 || random == 10){
+        alert(`nota: ${random} EXCELENTE`);
+    }
+    else if (random >= 4){
+        alert(`nota: ${random} APROBO`)
+    }
+    else if (random < 4){
+        alert(`nota: ${random} Vamos, la proxima se puede`)
+    }
+    else {
+        alert('Valor no contemplado')
+    }
 
 }//FIN DE LA FUNCIÓN
