365a7a44fa22644b9c1f88b858f433bce3705dcf
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 06:48:53 -0300

    E/S Ejerc 09

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	alert("ok");
+    // valor Sueldo
+    let sueldo = document.getElementById('txtIdSueldo').value;
+    sueldo = parseInt(sueldo,10);
+    
+    // Operacion y aumento
+    let aumento = sueldo * (10 / 100);
+    let sueldoFinal = sueldo + aumento;
+    
+    // Mostrar sueldoFinal
+    let objResultado = document.getElementById('txtIdResultado')
+    objResultado.placeholder = sueldoFinal;
 }
