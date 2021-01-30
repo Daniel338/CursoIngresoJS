6f87b636e6155c4b7cec618862e0c64dd4f8e734
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 15:00:05 -0300

    Instr IF 08

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,13 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
-	
-
-
+    let age = document.getElementById('txtIdEdad').value;
+    age = parseInt(age,10);
+    
+    // Tomo el estado civil
+    let civilStatus = document.getElementById('estadoCivil').value;
+    
+    if( age >= 18 && (civilStatus == 'Soltero')){
+        alert('Eres soltero y no eres menor de edad');
+    }
 }//FIN DE LA FUNCIÓN
