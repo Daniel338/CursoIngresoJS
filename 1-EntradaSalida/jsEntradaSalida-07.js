b42e8d871c623a68ee4077b8331f8ac6ecf61598
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 06:09:32 -0300

    E/S Ejerc 07

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,43 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+  let num1 = document.getElementById('txtIdNumeroUno').value;
+  let num2 = document.getElementById('txtIdNumeroDos').value;
+  // conversion
+  num1 = parseInt(num1,10);
+  num2 = parseInt(num2,10);
+  alert(`La suma es ${num1 + num2}`)
 }
 
 function restar()
 {
-	
+  let num1 = document.getElementById('txtIdNumeroUno').value;
+  let num2 = document.getElementById('txtIdNumeroDos').value;
+  // conversion
+  num1 = parseInt(num1,10);
+  num2 = parseInt(num2,10);
+  alert(`La Resta es ${num1 - num2}`)
 }
 
 function multiplicar()
 { 
-	
+  let num1 = document.getElementById('txtIdNumeroUno').value;
+  let num2 = document.getElementById('txtIdNumeroDos').value;
+  // conversion
+  num1 = parseInt(num1,10);
+  num2 = parseInt(num2,10);
+  alert(`La Multiplicacion es ${num1 * num2}`)
+
 }
 
 function dividir()
 {
-	
+  let num1 = document.getElementById('txtIdNumeroUno').value;
+  let num2 = document.getElementById('txtIdNumeroDos').value;
+  // conversion
+  num1 = parseInt(num1,10);
+  num2 = parseInt(num2,10);
+  alert(`La Division es ${num1 / num2}`)
+
 }
 
