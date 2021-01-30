a9d27c008a1bf108416ba46b6113a1f102fcea1c
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 15:01:11 -0300

    Instr IF 09

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,8 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+    let random = Math.random() * 10;
+    random = Math.floor(random + 1);
+    alert(random)
 
 }//FIN DE LA FUNCIÓN
