955769c7c3314b8ae9fd94f7bb6879e5a426e50d
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 18:55:54 -0300

    Instr Switch 03

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -3,7 +3,17 @@ function mostrar()
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
 	alert(mesDelAño);
+
+	let message1 = 'Este mes no tiene más de 29 días.'
+	let message2 = 'Este mes tiene 30 o más días.'
 	
+	switch(mesDelAño){
+		case 'Febrero':
+			alert(message1);
+			break;
+		default:
+			alert(message2);
+	}
 	
 
 
