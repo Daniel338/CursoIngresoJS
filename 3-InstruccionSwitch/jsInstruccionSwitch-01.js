f90002250e113bc0eb4436b25f814a6383006bda
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 17:18:59 -0300

    Instr Switch 01

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -3,7 +3,23 @@ function mostrar()
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
 	alert(mesDelAño);
-
+    
+    switch (mesDelAño){
+      case 'Enero':
+        alert('Que comiences bien el año!!!.');
+        break;
+      case 'Marzo':
+        alert('A clases!!!.');
+        break;
+      case 'Julio':
+        alert('Se vienen las vacaciones!!!.');
+        break;
+      case 'Diciembre':
+        alert('Felices Fiestas!!!.');
+        break;
+      default:
+        alert('Valor no contemplado');
+    }
 
 
 }//FIN DE LA FUNCIÓN
