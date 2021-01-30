48ae73b14ed8aa050cceaff9ae7313b14c20ffb8
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 07:05:09 -0300

    E/S Ejerc 10 y cambio en html mostrarAumento() por mostrarDescuento()

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,7 +3,17 @@ Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
+function mostrarDescuento()
 {
-	alert("ok");
+    // valor importe
+    let importe = document.getElementById('txtIdImporte').value;
+    importe = parseInt(importe,10);
+    
+    // Operacion y descuento
+    let descuento = importe * (25 / 100);
+    let sueldoFinal = importe - descuento;
+    
+    // Mostrar sueldoFinal
+    let objResultado = document.getElementById('txtIdResultado')
+    objResultado.placeholder = sueldoFinal;
 }
