06ddb2c3b835c43562ef132726de50302da95d46
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 14:59:22 -0300

    Instr IF 06

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,24 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
-
-
+    let age = document.getElementById('txtIdEdad').value;
+    age = parseInt(age,10);
+    
+    // mayor de edad
+    if ( age >= 18){
+        alert('Eres mayor de edad');
+    }
+    // Adolecente
+    else if (age >= 13 && age <= 17){
+        alert('Eres un adolecente');
+    }
+    // Niño
+    else if ( age >= 6 && age <= 13){
+        alert('Eres un niño');
+    } 
+    // is not a namber
+    else {
+        alert('Valor no contemplado');
+    }
 
 }//FIN DE LA FUNCIÓN
