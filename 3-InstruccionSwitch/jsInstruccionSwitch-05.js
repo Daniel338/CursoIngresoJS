c441cb6bddf0db96ccf656612ab68dfc2a861728
DanielAlejo <danielalejo131@gmail.com>
2021-01-30 19:48:56 -0300

    Instr Switch 05

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -2,9 +2,30 @@ function mostrar()
 {
 	//tomo la hora
 	var horaDelDia =txtIdHora.value;
+	horaDelDia = parseInt(horaDelDia,10);
 	alert(horaDelDia);
+
+	let message1 = `Es de mañana`;
 	
-	
+	switch(horaDelDia){
+		case 7:
+			alert(message1);
+			break;
+		case 8:
+			alert(message1);
+			break;
+		case 9:
+			alert(message1);
+			break;
+		case 10:
+			alert(message1);
+			break;
+		case 11:
+			alert(message1);
+			break;
+		default:
+			alert('valor no contemplado');
+	}
 
 
 
