6ec312d8914c860be5ed5954cc8c0d584f7d3d51
DanielAlejo <danielalejo131@gmail.com>
2021-01-31 09:51:12 -0300

    Instr Switch 07

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -3,4 +3,22 @@ function mostrar()
 	var destinoIngresado =txtIdDestino.value;
 	alert(destinoIngresado);
 
+	// Punto de referencia Buenos Aires
+	switch(destinoIngresado){
+		case 'Bariloche':
+			alert('Oeste');
+			break;
+		case 'Cataratas':
+			alert('Norte');
+			break;
+		case 'Mar del plata':
+			alert('Sur');
+			break;
+		case 'Ushuaia':
+			alert('Sur');
+			break;
+		default:
+			alert('Valor no contemplado');
+	}
+
 }//FIN DE LA FUNCIÓN
