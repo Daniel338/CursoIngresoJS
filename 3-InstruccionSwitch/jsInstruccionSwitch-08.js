3d620e89ee109e3fce394bbbceb2f1762ec49a78
DanielAlejo <danielalejo131@gmail.com>
2021-01-31 10:03:02 -0300

    Instr Switch 08

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -3,4 +3,21 @@ function mostrar()
 	var destinoIngresado =txtIdDestino.value;
 	alert(destinoIngresado);
 
+	switch(destinoIngresado){
+		case 'Bariloche':
+			alert(`En ${destinoIngresado} hace Frio`);
+			break;
+		case 'Cataratas':
+			alert(`En ${destinoIngresado} hace Calor`);
+			break;
+		case 'Mar del plata':
+			alert(`En ${destinoIngresado} hace Calor`);
+			break;
+		case 'Ushuaia':
+			alert(`En ${destinoIngresado} hace Frio`);
+			break;
+		default:
+			alert('Valor no contemplado');
+	}
+
 }//FIN DE LA FUNCIÓN
