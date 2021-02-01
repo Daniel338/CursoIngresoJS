9d0a01e0ef6440146586a431fdabafaf3a2167c0
DanielAlejo <danielalejo131@gmail.com>
2021-02-01 09:23:09 -0300

    Instr Switch 09

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,61 @@
 function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+{	// Tomo estacion del año y destino
+	let estacion = document.getElementById("txtIdEstacion").value;
+	let destino = document.getElementById("txtIdDestino").value;
 
-}//FIN DE LA FUNCIÓN
+	// Precios
+	let base = 15000;
+	let porcentaje1 = base * 20/100;
+	let porcentaje2 = base * 10/100;
+
+	switch(estacion){
+		case 'Invierno':
+			switch(destino){
+				case 'Bariloche':
+					alert(`Precio Final: ${base + porcentaje1}`);
+					break;
+				case 'Cataratas': 
+				case 'Cordoba':
+					alert(`Precio Final: ${base - porcentaje2}`);
+					break;
+				case 'Mar del plata':
+					alert(`Precio Final: ${base - porcentaje1}`);
+					break;
+				default:
+					alert('Valor no contemplado en destino');
+			}
+			break;
+		case 'Verano':
+			switch(destino){
+				case 'Bariloche':
+					alert(`Precio Final: ${base - porcentaje1}`);
+					break;
+				case 'Cataratas':
+				case 'Cordoba':
+					alert(`Precio Final: ${base + porcentaje2}`);
+					break;
+				case 'Mar del plata':
+					alert(`Precio Final: ${base + porcentje1}`);
+					break;
+				default:
+					alert('Valor no contemplado en destino');
+			}
+			break;
+		case 'Otoño':
+		case 'Primavera':
+			switch(destino){
+				case 'Bariloche':
+				case 'Cataratas':
+				case 'Mar del plata':
+					alert(`Precio Final: ${base + porcentje2}`);
+					break;
+				case 'Cordoba':
+					alert(`Precio Final: ${base}`);
+				default:
+					alert('Valor no contemplado en destino');
+			}
+		break;
+		default:
+			alert('Valor no contemplado en estacion');
+	}
+}
