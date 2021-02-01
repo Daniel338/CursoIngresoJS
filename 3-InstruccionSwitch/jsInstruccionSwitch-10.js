bdf10801883d6226d9f138d530098dab4a703f12
DanielAlejo <danielalejo131@gmail.com>
2021-02-01 09:43:06 -0300

    Instr Switch 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,49 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar(){
+	// Tomo estacion y destino
+	let estacion = document.getElementById("txtIdEstacion").value;
+	let destino = document.getElementById("txtIdDestino").value;
 
-}//FIN DE LA FUNCIÓN
+	switch(estacion){
+		case 'Invierno':
+			switch(destino){
+				case 'Bariloche':
+					alert('Se viaja');
+					break;
+				case 'Cataratas':
+				case 'Mar del plata':
+				case 'Cordoba':
+					alert('No se viaja');
+					break;
+				default:
+					alert('Valor no contemplado en destino');
+				}
+			break;
+		case 'Verano':
+			switch(destino){
+				case 'Cataratas':
+				case 'Mar del plata':
+					alert('Se viaja');
+					break;
+				case 'Bariloche':
+				case 'Cordoba':
+					alert('No se viaja');
+					break;
+				default:
+					alert('Valor no contemplado en destino');
+				}
+			break;
+		case 'Otoño':
+			alert('Se viaja');
+			break;
+		case 'Primavera':
+			if ( destino != 'Bariloche'){
+				alert('Se viaja')
+			}
+			else if ( destino == 'Bariloche'){
+				alert('No se viaja')
+			}
+			break;
+		default:
+			alert('Valor no contemplado en estacion');
+	}
+}
